a95a5778a8f5375f80fc25e848f8fc4898147027
HamidMammadov <hamidvm@code.edu.az>
2022-03-17 13:22:07 +0400

    First Commit

---
+++ Condition/script.js
@@ -0,0 +1,144 @@
+// let a  = parseInt(prompt("Birinci Deyeri Daxil Et:"));
+// let b  = parseInt(prompt("Ikinci Deyeri Daxil Et:"));
+
+// let sum = a+b;
+// console.log("sum: "+sum);
+// let result = sum % 5;
+// let result2 = sum % 4;
+// console.log(result);
+// let isdevidebyfive = result == 0;
+// let isdevidebyFour = result2 == 0;
+// console.log(isdevidebyfive);
+// console.log(isdevidebyFour);
+
+
+//Condition
+// let day = parseInt(prompt("Birinci Deyeri Daxil Et:"));
+
+// if (day == 1)
+// {
+//     console.log("Birinci");
+// }
+// else if(day == 2)
+// {
+//     console.log("Ikinci");
+
+// }
+// else if(day == 3)
+// {
+//     console.log("Ucuncu");
+
+// }
+// else if(day == 4)
+// {
+//     console.log("Dorduncu");
+
+// }
+// else if(day == 5)
+// {
+//     console.log("Besinci");
+
+// }else
+// {
+//     console.log("Duzgun Deyer Daxil Et")
+// }
+
+
+// switch (day) {
+//     case 1:
+//     console.log("Birinci");
+//         break;
+//     case 2:
+//     console.log("Ikinci");
+//         break;
+//     case 3:
+//     console.log("Ucuncu");
+//         break;
+//     case 4:
+//     console.log("Dorduncu");
+//         break;
+//     case 5:
+//     console.log("Besinci");
+//         break;
+//     default:
+//         console.log("Duzgun Daxil")
+//         break;
+// }
+
+
+//Loop
+// let min  = parseInt(prompt("Birinci Deyeri Daxil Et:"));
+// let max  = parseInt(prompt("Ikinci Deyeri Daxil Et:"));
+
+// let result = 0;
+
+// while(min <= max)
+// {
+//     if( min% 5 == 0)
+//     {
+//         result++
+//         console.log("result: "+result)
+//     }
+//     console.log("Min: "+min)
+//     min++;
+// }
+
+
+// do 
+// {
+//     if( min% 5 == 0)
+//     {
+//         result++
+//         console.log("result: "+result)
+//     }
+//     console.log("Min: "+min)
+//     min++;
+// } while (min <= max);
+
+// let i = 1;
+
+// while(i<=100)
+// {
+//     if(i % 2==0)
+//     {
+//         result+=i;
+//     }
+    
+//     i++;
+// }
+//Start
+// for (let i = 0; i <= 100; i++) 
+// {
+//     if(i % 2==0)
+//     {
+//         result+=i;
+//     }
+// }
+
+// for (let i = min; i <= max; i++) 
+// {
+//     if(i % 2==0)
+//     {
+//         result+=i;
+//     }
+// }
+//End
+// while(true){
+//     console.log("test")
+// }
+
+// console.log(result);
+
+let a = parseInt(prompt("Deyer Daxil Et"));
+
+a**2;
+
+if(a % 2 == 0)
+{
+    console.log("Cutdur");
+}
+else
+{
+    console.log("Tekdir");
+
+}
